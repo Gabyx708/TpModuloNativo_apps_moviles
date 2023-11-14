3a531a8fc48d18afbb967eb2d051e30d2203c84a
Gabriel <matiascoria427@gmail.com>
2023-11-14 17:40:50 -0300

    cambios en logica de donaciones

---
+++ app/src/main/java/com/example/tpmodulonativo/Controllers/DonationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tpmodulonativo.Controllers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tpmodulonativo.R
+
+class DonationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_donation)
+    }
+}
